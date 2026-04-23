b8bd86f6729d5e06d5796b3ef05efc4440b04633
Daki De Alwis <dakibrittain@gmail.com>
2026-04-23 22:50:52 +0200

    Add working select dropdown with ticking selected city clock

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let now = new Date();
+let selectedTimeZone = "";
+
 function updateClocks() {
-    let now = new Date();
+    now = new Date();
     document.getElementById("amsterdam-time").innerHTML = new Intl.DateTimeFormat("en-GB", {
      timeZone: "Europe/Amsterdam",
      hour: "2-digit",
@@ -27,7 +30,47 @@ function updateClocks() {
         month: "long",
         day: "numeric"
     }).format(now);
+
+    if (selectedTimeZone) {
+    document.getElementById("selected-city-name").innerHTML = selectedTimeZone;
+    document.getElementById("selected-time").innerHTML = new Intl.DateTimeFormat("en-GB", {
+        timeZone: selectedTimeZone,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    }).format(now);
+    document.getElementById("selected-date").innerHTML = new Intl.DateTimeFormat("en-GB", {
+        timeZone: selectedTimeZone,
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    }).format(now); 
+
+    }
 }
+
+document.getElementById("city-select").addEventListener("change", function() {
+    let timeZone = this.value;
+    selectedTimeZone = timeZone
+    console.log(timeZone);
+    document.getElementById("selected-clock").style.display = "block";
+    document.getElementById("selected-city-name").innerHTML = this.value;
+    document.getElementById("selected-time").innerHTML = new Intl.DateTimeFormat("en-GB", {
+        timeZone: selectedTimeZone,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    }).format(now);
+    document.getElementById("selected-date").innerHTML = new Intl.DateTimeFormat("en-GB", {
+        timeZone: selectedTimeZone,
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    }).format(now); 
+
+})
 
 setInterval(updateClocks, 1000);
 updateClocks();
